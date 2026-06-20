8d0888bb7bba6e496e0eb799788e94a1bc895aad
lencet-344 <odoricozeleherrera@gmail.com>
2026-06-20 09:45:28 -0600

    feat: create request Factory_articleRequest

---
+++ app/Http/Requests/Factory_articleRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class Factory_articleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'current_stock'=>'integer|required|unsigned',
+            'negotiation_cost'=>'integer|required|unsigned',
+            'date_estimated'=>"date|required|",
+        ];
+    }
+
+    public function messages():array
+    {
+        return[
+
+            'current_stock.integer'=>'El campo solo permite numeros enteros',
+            'current_stock.required'=>'El campo es requerido',
+            'current_stock.unsigned'=>'El campo solo permite numeros enteros',
+
+            'negotiation_cost.integer'=>'El costo de negociacion solo permite numeros enteros',
+            'negotiation_cost.required'=>'El campo es requerido',
+            'negotiation_cost.unsigned'=>'El campo solo permite numeros enteros',
+
+            'date_estimated.date'=>'El campo solo permite fecha estimada',
+            'date_estimated.required'=>'El campo es requerido'
+
+        ];
+    }
+}
