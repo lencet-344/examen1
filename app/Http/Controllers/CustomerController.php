d5873730ed909f6b9cb7a8910d5b996ffb5f6f56
lencet-344 <odoricozeleherrera@gmail.com>
2026-06-27 07:57:38 -0600

    feat: create controller CustomerController

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Customer;    
+use App\Http\Requests\CustomerRequest;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $customers = Customer::orderByDesc("id")->get();
+        return view("customers.index", compact("customers"));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $customer = new Customer();
+        return view("customer.create", compact("customer"));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CustomerRequest $request)
+    {
+        Customer::create($request->validated());
+        return redirect()->route("customers.index")->with("success", "El cliente se ha creado correctamente.");
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $customer = Customer::findOrFail($id);
+        return view("Customer.show", compact("customer"));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $customer = Customer::findOrFail($id);
+        return view("Customer.edit", compact("customer"));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(CustomerRequest $request, Customer $customer)
+    {
+        $customer->update($request->validated());
+        return redirect()->route("customers.index")->with("success", "El cliente se ha actualizado correctamente.");
+    }
+    
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $customer = Customer::findOrFail($id);
+        $customer->delete();
+        return redirect()->route("customers.index")->with("success", "El cliente se ha eliminado correctamente.");
+    }
+}
