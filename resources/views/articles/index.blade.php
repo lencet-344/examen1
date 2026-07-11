0f53fe0b419802cbf3b4dea5a20fd589704ac33d
lencet-344 <odoricozeleherrera@gmail.com>
2026-07-10 20:11:22 -0600

    feat:create view

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -91,7 +91,7 @@
                                             <svg class="w-12 h-12 text-gray-300 dark:text-gray-600 mb-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"></path></svg>
                                             <p class="text-gray-500 dark:text-gray-400 text-lg font-medium">No hay artículos registrados</p>
                                         </div>
-                                    </td>
+                                    </td>x
                                 </tr>
                             @endforelse
                         </tbody>
